5b591509f173c48771f545b93931314af0683ff9
mohammadloo_meysam <mohammadloo.developer@gmail.com>
2022-03-04 14:10:24 +0330

    build navbar

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var barClick = document.getElementById('bar-btn')
+var close_Bar_Btn = document.getElementById('close-bar-btn')
+var ulVar = document.getElementById('nav-id')
+
+//.............................................................
+//barClick.addEventListener('click',)
+function barFunction() {
+    ulVar.classList.add('nav-class2')
+    close_Bar_Btn.classList.add('btn-bar-class-close2')
+    barClick.classList.add('btn-bar-class2')
+}
+//.............................................................
+//close_Bar_Btn.addEventListener('click',)
+function closeBarFunction() {
+    ulVar.classList.remove('nav-class2')
+    close_Bar_Btn.classList.remove('btn-bar-class-close2')
+    barClick.classList.remove('btn-bar-class2')
+}
+
